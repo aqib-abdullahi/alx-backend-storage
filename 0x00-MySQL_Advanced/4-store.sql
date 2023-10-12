fab4aa23c5c812162637e76d32f02b3de9caa477
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-10-12 22:19:05 +0000

    ahndles error if it exists task4

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,12 +1,12 @@
 -- increates a trigger that decreases the quantity of an item
 -- after adding a new order. Quantity in the table items can be negative.
 DELIMITER $$
-CREATE TRIGGER upade_item
+CREATE TRIGGER IF NOT EXISTS update_item $$
 AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
 	UPDATE
 		SET quantity = quantity - NEW.number
 		WHERE name = NEW.item_name;
-END$$
+END $$
 DELIMITER ;
