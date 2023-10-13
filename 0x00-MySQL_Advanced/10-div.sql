bf37a3331d41261c929bae9874ae97313245c03d
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-10-13 06:01:09 +0000

    safeDiv task10

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,15 @@
+-- safeDiv divides and returns fist by the second number
+-- or 0 if the second number is 0
+DROP FUNCTION IF EXISTS SafeDiv;
+DELIMITER $$
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
+BEGIN
+    DECLARE result FLOAT DEFAULT 0;
+
+    IF b != 0 THEN
+        SET result = a / b;
+    END IF;
+    RETURN result;
+END $$
+DELIMITER ;
