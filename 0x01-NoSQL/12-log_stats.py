5372c235030d970bac45021abb58b05263f8c15d
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-10-17 00:13:16 -0700

    task12

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Nginx log stats
+"""
+from pymongo import MongoClient
+
+
+from pymongo import MongoClient
+
+def count_logs_with_method(mongo_collection, method):
+    return mongo_collection.count_documents({"method": method})
+
+def count_logs_with_status_check(mongo_collection):
+    return mongo_collection.count_documents({"method": "GET", "path": "/status"})
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs_collection = client.logs.nginx
+
+    total_logs = logs_collection.count_documents({})
+    get_count = count_logs_with_method(logs_collection, "GET")
+    post_count = count_logs_with_method(logs_collection, "POST")
+    put_count = count_logs_with_method(logs_collection, "PUT")
+    patch_count = count_logs_with_method(logs_collection, "PATCH")
+    delete_count = count_logs_with_method(logs_collection, "DELETE")
+    status_check_count = count_logs_with_status_check(logs_collection)
+
+    print(f"{total_logs} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {get_count}")
+    print(f"\tmethod POST: {post_count}")
+    print(f"\tmethod PUT: {put_count}")
+    print(f"\tmethod PATCH: {patch_count}")
+    print(f"\tmethod DELETE: {delete_count}")
+    print(f"{status_check_count} status check")
