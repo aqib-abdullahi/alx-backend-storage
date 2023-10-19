24390072ead7196d6db0cf492c69c653da351b13
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-10-18 23:13:27 -0700

    task2 added methods

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Cache module task0
 """
-from typing import Union
+from typing import Union, Callable
 import redis
 import uuid
 
@@ -22,3 +22,23 @@ class Cache():
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
+
+    def get(key: str, fn: Callable = None):
+        """takes a key string argument and an optional callable
+        which will be used to convert data back to the desired
+        format"""
+        data = self._redis.get(key)
+        if data and fn is not None:
+            fn(data)
+            return data
+        else:
+            return data
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """parametrize Cache.get with the correct conversion
+        function.
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        return self.get(key, fn=lambda d: int(d))
